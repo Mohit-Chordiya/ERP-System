247329064418249ab11e44b26053b115a12c9a6e
Mohit-Chordiya <mohitmchordiya33@gmail.com>
2024-03-14 23:29:54 +0530

    Make the site responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import Main_Screen from "./components/Main/Main-Screen";
-import { useEffect } from "react";
 function App() {
-    useEffect(() => {
-        document.body.style.overflow = "hidden";
-        return () => {
-            document.body.style.overflow = "scroll";
-        };
-    }, []);
     return <Main_Screen />;
 }
 
